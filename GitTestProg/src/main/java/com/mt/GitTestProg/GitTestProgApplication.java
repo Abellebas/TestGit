5ab0f27bc8cea2fea12c11f62830a62508fbc02b
Abellebas <abellebas1999@gmail.com>
2021-05-24 10:43:07 +0530

    New project Commited

---
+++ GitTestProg/src/main/java/com/mt/GitTestProg/GitTestProgApplication.java
@@ -0,0 +1,13 @@
+package com.mt.GitTestProg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestProgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestProgApplication.class, args);
+	}
+
+}
