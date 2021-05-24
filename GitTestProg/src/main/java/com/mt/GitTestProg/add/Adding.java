9fc54b80d22ab064caf1bb95d7fd5b8af8345c45
Abellebas <abellebas1999@gmail.com>
2021-05-24 12:45:59 +0530

    new branch

--- GitTestProg/src/main/java/com/mt/GitTestProg/add/Adding.java
+++ GitTestProg/src/main/java/com/mt/GitTestProg/add/Adding.java
@@ -3,12 +3,6 @@ package com.mt.GitTestProg.add;
 public class Adding {
 
 	public static void main(String[] args) {
-		System.out.println(5+5);
-		
-		System.out.println("In branch chennai");
-		System.out.println(3);
-
-		System.out.println(5+"master");
-		System.out.println(4);
+System.out.println("new branch");
 	}
 }
