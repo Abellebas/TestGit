f0618ffd15cd3a7149121a88fb4b58bc26fa643a
Abellebas <abellebas1999@gmail.com>
2021-05-24 10:54:32 +0530

    updating

---
+++ GitTestProg/src/main/java/com/mt/GitTestProg/add/Adding.java
@@ -0,0 +1,8 @@
+package com.mt.GitTestProg.add;
+
+public class Adding {
+
+	public static void main(String[] args) {
+		System.out.println(5+5);
+	}
+}
---
+++ GitTestProg/src/main/java/com/mt/GitTestProg/subract/Subracting.java
@@ -0,0 +1,7 @@
+package com.mt.GitTestProg.subract;
+
+public class Subracting {
+	public static void main(String[] args) {
+		System.out.println(10-5);
+	}
+}
